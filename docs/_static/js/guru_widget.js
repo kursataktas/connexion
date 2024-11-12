c3861aa8b2e52bdc3f22c308e043ef15b37712c9
Kürşat Aktaş <kursat.ce@gmail.com>
2024-11-12 18:25:29 +0300

    change margin

--- docs/_static/js/guru_widget.js
+++ docs/_static/js/guru_widget.js
@@ -4,6 +4,6 @@ document.write(`
         data-link="https://gurubase.io/g/connexion" 
         data-bg-color="#64B45C" 
         data-icon-url="https://gurubase.io/_next/image?url=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F58754196%3Fv%3D4&w=96&q=75"
-        data-margins='{"bottom": "3rem", "right": "1rem"}'>
+        data-margins='{"bottom": "8rem", "right": "1rem"}'>
     </script>
 `);
